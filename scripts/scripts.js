8e19df36106a560a40fb7f79b41c6309f0bfd1a9
wedemosaem <152498519+wedemosaem@users.noreply.github.com>
2025-05-13 14:44:59 +0200

    Update scripts.js
    include generate variations on project sidekick

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -273,6 +273,9 @@ async function loadLazy(doc) {
   } else {
     loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   }
+  
+  import('../tools/sidekick/aem-genai-variations.js');
+  
   addFavIcon(`${window.wknd.demoConfig.demoBase || window.hlx.codeBasePath}/favicon.png`);
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
